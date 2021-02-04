8241da58cc6ee9116b1961fee60de21321e61b52
mohamedatiah <mohamedatiah1997@gmail.com>
2021-02-04 19:14:44 +0200

    finishedDesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AddPostComponent } from './add-post/add-post.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
   const routes: Routes = [
     {path:'home',component:HomePageComponent},
     {path:"profile",component:ProfileComponent},
-    {path:"addpost",component:AddPostComponent}
+    {path:"addpost",component:AddPostComponent},
+    {path:"contact",component:ContactComponent},
+    {path:"register",component:RegisterComponent},
+    {path:"login",component:LoginComponent},
   
   ]
 
