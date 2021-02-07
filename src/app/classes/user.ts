c298ba9eb75461631bcc0d61a26bb693e90b55ec
mohamedatiah <mohamedatiah1997@gmail.com>
2021-02-07 15:57:11 +0200

    register

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,6 +1,6 @@
 export class User {
     constructor(public _id:string,public  username:string,public password:string,public firstname:string
-        ,public lastname:string,public email:string,public age:number,followers:User,followings:User,
+        ,public lastname:string,public email:string,public age:number,followers:string,followings:string,
         public image:string,public token:string
         )
         {}
